8a6bbc0c57d534fbb4878bbed0df327649893ed2
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2024-12-02 21:38:58 +0500

    add login

--- backend/controllers/authController.mjs
+++ backend/controllers/authController.mjs
@@ -1,22 +1,47 @@
 import userModel from "../models/userModel.mjs";
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
 
 
 export const register = async(req,res)=>{
   try{
     let {fullname,email,password} = req.body;
+    let hash = await bcrypt.hash(password,10);
     let user = await userModel.create({
       fullname,
       email,
-      password
+      password : hash
     })
+    let token = jwt.sign({email,userId:user._id},process.env.SECRET_KEY);
     res.status(200).json({
+      token,
       user,
       message : 'user created',
       success : true
     })
   }catch(err){
+    return res.status(200).json({
+      message : err.message,
+      success : false
+    })
+  }
+}
+export const login = async(req,res)=>{
+  try{
+    let {email,password} = req.body;
+    let user = await userModel.findOne({email});
+    let userExists = await bcrypt.compare(password,user.password);
+    let token = jwt.sign({email,userId:user._id},process.env.SECRET_KEY);
     res.status(200).json({
-      message : error.message,
+      token,
+      user,
+      message : 'user login',
+      success : true
+    })
+  }catch(err){
+    return res.status(200).json({
+      message : err.message,
       success : false
     })
   }
--- backend/routes/userRouter.mjs
+++ backend/routes/userRouter.mjs
@@ -1,8 +1,9 @@
 import express from "express";
-import { register } from "../controllers/authController.mjs";
+import { login, register } from "../controllers/authController.mjs";
 
 let router = express.Router();
 
 
 router.post('/create',register)
+router.post('/login',login)
 export default router
