f817e168d2a6355a22de4db2b42133bedad3e1cc
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2025-05-14 19:41:44 +0500

    feat: update API endpoints to use Vercel deployment URLs and adjust CORS settings

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,6 +1,9 @@
 
 
 export const USER_API_END_POINT = 'https://deploymernapp02-72u3z2lty-muhammadayaz77s-projects.vercel.app';
+
 export const JOB_API_END_POINT = 'https://deploymernapp02-72u3z2lty-muhammadayaz77s-projects.vercel.app/job';
+
 export const APPLICATIONS_API_END_POINT = 'https://deploymernapp02-72u3z2lty-muhammadayaz77s-projects.vercel.app/application';
+
 export const COMPANY_API_END_POINT = 'https://deploymernapp02-72u3z2lty-muhammadayaz77s-projects.vercel.app/company';
